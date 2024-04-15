69e0543b0ac3a7414bb62bc96173b1035eee3e35
Navnath71 <navnathsangale71@gmail.com>
2024-04-15 12:40:50 +0530

    second commit

--- project/backend/index.js
+++ project/backend/index.js
@@ -1,68 +1,50 @@
 const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
-const FormDataModel = require ('./models/FormData');
-
-
-app.use(express.json());
-const _dirname=path.dirname()
-const buildpath=path.join(_dirname,"../client/build")
-app.use(express.static(buildpath));
-app.use(
-    cors({
-        "origin": "*",
-    })
-);
-app.use(userroutes);
-app.use(productroute);
-
+const FormDataModel = require('./models/FormData');
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb+srv://vaishnavi2019pandure:qFX7XLiR1zWFvGR2@cluster0.oopj2nf.mongodb.net/new');
+// Define routes after app initialization
 
-app.post('/register', (req, res)=>{
-    // To post / insert data into database
-
-    const {email, password} = req.body;
-    FormDataModel.findOne({email: email})
-    .then(user => {
-        if(user){
-            res.json("Already registered")
-        }
-        else{
-            FormDataModel.create(req.body)
-            .then(log_reg_form => res.json(log_reg_form))
-            .catch(err => res.json(err))
-        }
-    })
-    
-})
-
-app.post('/login', (req, res)=>{
-    // To find record from the database
-    const {email, password} = req.body;
-    FormDataModel.findOne({email: email})
-    .then(user => {
-        if(user){
-            // If user found then these 2 cases
-            if(user.password === password) {
-                res.json("Success");
+app.post('/register', (req, res) => {
+    const { email, password } = req.body;
+    FormDataModel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                res.json("Already registered");
+            } else {
+                FormDataModel.create(req.body)
+                    .then(log_reg_form => res.json(log_reg_form))
+                    .catch(err => res.json(err));
             }
-            else{
-                res.json("Wrong password");
-            }
-        }
-        // If user not found then 
-        else{
-            res.json("No records found! ");
-        }
-    })
-})
-
-app.listen(3001, () => {
-    console.log("Server listining on http://127.0.0.1:3001");
-
+        });
 });
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    FormDataModel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                if (user.password === password) {
+                    res.json("Success");
+                } else {
+                    res.json("Wrong password");
+                }
+            } else {
+                res.json("No records found!");
+            }
+        });
+});
+
+// Connect to MongoDB after app initialization
+mongoose.connect('mongodb+srv://vaishnavi2019pandure:qFX7XLiR1zWFvGR2@cluster0.oopj2nf.mongodb.net/new')
+    .then(() => {
+        app.listen(3001, () => {
+            console.log("Server listening on http://127.0.0.1:3001");
+        });
+    })
+    .catch(err => console.error('Error connecting to MongoDB:', err.message));
