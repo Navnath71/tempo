2e83e855add1539822fa67ff49e49c6ad011f75c
Navnath71 <navnathsangale71@gmail.com>
2024-04-15 22:58:27 +0530

    commit 7

--- project/backend/frontend/src/components/Register.jsx
+++ project/backend/frontend/src/components/Register.jsx
@@ -13,7 +13,7 @@ const Register = () => {
     const handleSubmit = (event) => {
         event.preventDefault();
         
-        axios.post( 'http://0.0.0.0/0:3000/register', {name, email, password})
+        axios.post( 'http://localhost:3000/register', {name, email, password})
         .then(result => {
             console.log(result);
             if(result.data === "Already registered"){
